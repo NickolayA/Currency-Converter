8cc2853ee62e5373704b81295d6c4c24b61db52d
Vipz3 <vipz3@mail.ru>
2018-06-18 13:30:07 +0300

    props.state.date has become props.date, possible better for reusability

--- components/ButtonDatePicker.js
+++ components/ButtonDatePicker.js
@@ -4,7 +4,7 @@ import {Text, View, Button } from 'react-native';
 const ButtonDatePicker = (props) => {
     return (
         <View>
-            <Text style={{alignSelf: 'center'}}>{`${props.state.date.getFullYear()}-${props.state.date.getMonth()+1}-${props.state.date.getDate()}`}</Text>
+            <Text style={{alignSelf: 'center'}}>{`${props.date.getFullYear()}-${props.date.getMonth()+1}-${props.date.getDate()}`}</Text>
             <Button 
                 title="Выбрать дату"
                 style={{alignSelf: 'stretch'}} 
