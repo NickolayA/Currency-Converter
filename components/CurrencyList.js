79f42b50454d996b60bf33fcda20ca3e827707f0
Vipz3 <vipz3@mail.ru>
2018-06-20 14:54:24 +0300

    Color of CurrencyList's head was changed to red

--- components/CurrencyList.js
+++ components/CurrencyList.js
@@ -5,7 +5,7 @@ import Card from './Card';
 const CurrencyList = (props) => {
    return (
        <View style={{flex: 1}}>
-            <Card cn="Наименование иностранной валюты" cs="Количество единиц иностранной валюты, буквенный код валюты" cor="Официальный курс" />
+            <Card style={{backgroundColor: 'red', color: 'white'}} cn="Наименование иностранной валюты" cs="Количество единиц иностранной валюты, буквенный код валюты" cor="Официальный курс" />
             <FlatList 
                 data={props.result}
                 renderItem={({item}) => <Card cn={item['Cur_Name']} cs={item['Cur_Scale']} cor={item['Cur_OfficialRate']} />}
